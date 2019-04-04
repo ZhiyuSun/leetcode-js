113a6d37d4e7e28a43017017ae3c16d1fce31067
sunzhiyu <sunzhiyu94@163.com>
2019-04-04 18:07:06 +0800

    修复种花问题中，未对右边界进行考虑的bug

--- code/array/lesson3.js
+++ code/array/lesson3.js
@@ -1,9 +1,12 @@
 export default (arr, n) => {
   // 计数器
   let max = 0
-  for (let i = 0, len = arr.length - 1; i < len; i++) {
+  for (let i = 0, len = arr.length; i < len; i++) {
     if (arr[i] === 0) {
       if (i === 0 && arr[1] === 0) {
+        max++
+        i++
+      } else if (i === len - 1 && arr[len - 2] === 0) { // 修复bug，教程里未考虑右边界的情况
         max++
         i++
       } else if (arr[i - 1] === 0 && arr[i + 1] === 0) {
